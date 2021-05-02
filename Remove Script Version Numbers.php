738e583ae27e61169d9312e9406baf17647bd434
Martin Maiolo <35736622+martinmaiolo@users.noreply.github.com>
2021-05-02 02:24:52 -0300

    Create Remove Script Version Numbers.php

---
+++ Remove Script Version Numbers.php
@@ -0,0 +1,8 @@
+function wpex_remove_script_version( $src ) {
+	if ( strpos( $src, 'ver=' ) ) {
+		$src = remove_query_arg( 'ver', $src );
+	}
+	return $src;
+}
+add_filter( 'script_loader_src', 'wpex_remove_script_version', 15, 1 );
+add_filter( 'style_loader_src', 'wpex_remove_script_version', 15, 1 );
